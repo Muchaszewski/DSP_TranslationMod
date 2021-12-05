5c54141de33cf9d8f7cccecda3ea3090ca6e87a7
kremnev8 <kremnev8super@yandex.ru>
2021-12-05 14:28:17 +0300

    Increment version.

--- DSPTranslationPlugin/TranslationPlugin.cs
+++ DSPTranslationPlugin/TranslationPlugin.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 namespace DSPTranslationPlugin
 {
-    [BepInPlugin("com.muchaszewski.dsp_translationPlugin", "DSP Community Translation", "0.3.0")]
+    [BepInPlugin("com.muchaszewski.dsp_translationPlugin", "DSP Community Translation", "0.4.0")]
     public class TranslationPlugin : BaseUnityPlugin
     {
         public static MonoBehaviour StaticMonoBehaviour { get; private set; }
